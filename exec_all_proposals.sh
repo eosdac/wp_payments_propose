6591149328121743f5186942361eec25726501ba
Michael Yeates <michaeljyeates@gmail.com>
2018-08-02 14:57:09 +0100

    Add scripts to cancel or exec all proposals

---
+++ exec_all_proposals.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROPOSER=[account name of the proposer]
+EXECUTOR=$PROPOSER
+URL=https://eu.eosdac.io
+CLEOS="cleos -u $URL"
+
+data=$($CLEOS get table eosio.msig $PROPOSER proposal -l 50)
+for row in $(echo "${data}" | jq -r '.rows[] | @base64'); do
+	_jq() {
+		echo ${row} | base64 --decode | jq -r ${1}
+	}
+
+	prop=$(_jq '.proposal_name')
+
+	echo "Executing $PROPOSER:$prop"
+
+	cmd="$CLEOS multisig exec $PROPOSER $prop $EXECUTOR"
+	echo $cmd
+	res=$(eval $cmd)
+done
